0dd64e0f9086480641f501f8ef63d626fee13872
Tu Ngo Minh <tungominh.hust@gmail.com>
2023-10-20 00:14:19 +0700

    Complete simple calculator app

--- ScientificCalculator/app/src/main/java/com/mtu/scientificcalculator/MainActivity.kt
+++ ScientificCalculator/app/src/main/java/com/mtu/scientificcalculator/MainActivity.kt
@@ -1,46 +1,224 @@
 package com.mtu.scientificcalculator
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mtu.scientificcalculator.ui.theme.ScientificCalculatorTheme
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mtu.scientificcalculator.databinding.MainLayoutBinding
 
-class MainActivity : ComponentActivity() {
+import com.mtu.scientificcalculator.logic.Calculator
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mainLayout: MainLayoutBinding
+    val cal = Calculator()
+    var openBrac = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ScientificCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        mainLayout = MainLayoutBinding.inflate(layoutInflater)
+        val view = mainLayout.root
+
+        var tvSec: TextView = mainLayout.secondTv // for display previous equation
+        var tvPrim: TextView = mainLayout.primaryTv // for display current equation or res
+        var btnAc: Button = mainLayout.btnAc
+        var btnC: Button = mainLayout.btnC
+        var btnOpenBrac: Button = mainLayout.btnOpBrac // open bracket
+        var btnClsBrac: Button = mainLayout.btnClsBrac // close bracket
+        var btnSq: Button = mainLayout.btnSq
+        var btnSqrt: Button = mainLayout.btnSqrt
+        var btnInv: Button = mainLayout.btnInv // inverse
+
+        var btnMultiple: Button = mainLayout.btnMultiple
+        var btnDiv: Button = mainLayout.btnDiv
+        var btnPlus: Button = mainLayout.btnPlus
+        var btnMinus: Button = mainLayout.btnMinus
+        var btnEqual: Button = mainLayout.btnEqual
+
+        var btn0: Button = mainLayout.btn0
+        var btn1: Button = mainLayout.btn1
+        var btn2: Button = mainLayout.btn2
+        var btn3: Button = mainLayout.btn3
+        var btn4: Button = mainLayout.btn4
+        var btn5: Button = mainLayout.btn5
+        var btn6: Button = mainLayout.btn6
+        var btn7: Button = mainLayout.btn7
+        var btn8: Button = mainLayout.btn8
+        var btn9: Button = mainLayout.btn9
+        var btnPi: Button = mainLayout.btnPi
+        var btnDot: Button = mainLayout.btnDot
+
+        // append character to primary textView
+        btn0.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "0"
+            tvPrim.text = strTemp
+        }
+
+        btn1.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "1"
+            tvPrim.text = strTemp
+        }
+
+        btn2.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "2"
+            tvPrim.text = strTemp
+        }
+
+        btn3.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "3"
+            tvPrim.text = strTemp
+        }
+
+        btn4.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "4"
+            tvPrim.text = strTemp
+        }
+
+        btn5.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "5"
+            tvPrim.text = strTemp
+        }
+
+        btn6.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "6"
+            tvPrim.text = strTemp
+        }
+
+        btn7.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "7"
+            tvPrim.text = strTemp
+        }
+
+        btn8.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "8"
+            tvPrim.text = strTemp
+        }
+
+        btn9.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "9"
+            tvPrim.text = strTemp
+        }
+
+        // check if there is dot before add to tv
+        btnDot.setOnClickListener {
+            val str = tvPrim.text.toString()
+            if(str.get(index = str.length - 1) != '.'){
+                val strTemp = tvPrim.text.toString() + "."
+                tvPrim.text = strTemp
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        btnOpenBrac.setOnClickListener {
+            val strTemp = tvPrim.text.toString() + "("
+            openBrac += 1
+            tvPrim.text = strTemp
+        }
+        btnClsBrac.setOnClickListener {
+            if(openBrac > 0){
+                val strTemp = tvPrim.text.toString() + ")"
+                openBrac -= 1
+                tvPrim.text = strTemp
+            } else{
+                Toast.makeText(this, "There is no open bracket", Toast.LENGTH_SHORT).show()
+            }
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScientificCalculatorTheme {
-        Greeting("Android")
+        btnMultiple.setOnClickListener {
+            val str = tvPrim.text.toString()
+            if(str.get(index = str.length - 2) != '*'){
+                val strTemp = tvPrim.text.toString() + "*"
+                tvPrim.text = strTemp
+            }
+        }
+        btnDiv.setOnClickListener {
+            val str = tvPrim.text.toString()
+            if(str.get(index = str.length - 2) != '/'){
+                val strTemp = tvPrim.text.toString() + "/"
+                tvPrim.text = strTemp
+            }
+        }
+        btnPlus.setOnClickListener {
+            val str = tvPrim.text.toString()
+            if(str.get(index = str.length - 2) != '+'){
+                val strTemp = tvPrim.text.toString() + "+"
+                tvPrim.text = strTemp
+            }
+        }
+        btnMinus.setOnClickListener {
+            val str = tvPrim.text.toString()
+            if(str.get(index = str.length - 2) != '-'){
+                val strTemp = tvPrim.text.toString() + "-"
+                tvPrim.text = strTemp
+            }
+        }
+
+
+        btnInv.setOnClickListener {
+            val dbTemp = tvPrim.text.toString().toDouble()
+            val res = 1 / dbTemp
+            val strRes = res.toString()
+            val strSec = "1/" + tvPrim.text.toString()
+            tvPrim.text = strRes
+            tvSec.text = strSec
+        }
+
+        // clear all textView
+        btnAc.setOnClickListener {
+            tvPrim.setText("")
+            tvSec.setText("")
+            openBrac = 0
+        }
+
+        // clear the last character in primary text view
+        btnC.setOnClickListener {
+            var str = tvPrim.text.toString()
+            if (str != ""){
+                if (str[str.length - 1] == ')') openBrac += 1
+                else if (str[str.length - 1] == '(') openBrac -= 1
+                str = str.substring(0, str.length - 2)
+                tvPrim.text = str
+            }
+        }
+
+        // add pi to current value
+        btnPi.setOnClickListener {
+            val str = tvPrim.text.toString() + "π"
+            tvPrim.text = str
+            tvSec.text = btnPi.text.toString()
+        }
+
+        btnSq.setOnClickListener {
+            val dbTemp = tvPrim.text.toString().toDoubleOrNull()
+            if (dbTemp != null){
+                val res = dbTemp.pow(2)
+                val strRes = res.toString()
+                val strSec = tvPrim.text.toString() + "^2"
+                tvPrim.text = strRes
+                tvSec.text = strSec
+            } else{
+                Toast.makeText(this, "Please enter valid number", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnSqrt.setOnClickListener {
+            val dbTemp = tvPrim.text.toString().toDoubleOrNull()
+            if (dbTemp != null){
+                val res = sqrt(dbTemp)
+                val strRes = res.toString()
+                val strSec = "√(" + tvPrim.text.toString() + ")"
+                tvPrim.text = strRes
+                tvSec.text = strSec
+            } else{
+                Toast.makeText(this, "Please enter valid number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnEqual.setOnClickListener {
+            val result: Double = cal.evaluateMathExpression(tvPrim.text.toString())
+            tvSec.text = tvPrim.text
+            tvPrim.text = result.toString()
+        }
+
+        setContentView(view)
     }
 }
--- ScientificCalculator/app/src/main/java/com/mtu/scientificcalculator/logic/Calculator.kt
+++ ScientificCalculator/app/src/main/java/com/mtu/scientificcalculator/logic/Calculator.kt
@@ -1,4 +1,67 @@
 package com.mtu.scientificcalculator.logic
 
+import java.util.Stack
+
 class Calculator {
+    fun evaluateMathExpression(expression: String): Double {
+        val operators = "+-*/"
+        val precedence = mapOf('+' to 1, '-' to 1, '*' to 2, '/' to 2)
+
+        fun isOperator(c: Char) = c in operators
+        fun getPrecedence(c: Char) = precedence[c] ?: 0
+
+        fun applyOperator(operators: Stack<Char>, values: Stack<Double>) {
+            val b = values.pop()
+            val a = values.pop()
+            val operator = operators.pop()
+
+            when (operator) {
+                '+' -> values.push(a + b)
+                '-' -> values.push(a - b)
+                '*' -> values.push(a * b)
+                '/' -> values.push(a / b)
+            }
+        }
+
+        val operatorStack = Stack<Char>()
+        val valueStack = Stack<Double>()
+
+        var i = 0
+        while (i < expression.length) {
+            val c = expression[i]
+            if (c in '0'..'9' || (c == '.' && i + 1 < expression.length && expression[i + 1] in '0'..'9')) {
+                // Parse the number
+                val start = i
+                while (i < expression.length && (expression[i] in '0'..'9' || expression[i] == '.')) {
+                    i++
+                }
+                val number = expression.substring(start, i).toDouble()
+                valueStack.push(number)
+            } else if (isOperator(c)) {
+                // Process operators
+                while (operatorStack.isNotEmpty()  && getPrecedence(operatorStack.peek()) >= getPrecedence(c)) {
+                    applyOperator(operatorStack, valueStack)
+                }
+                operatorStack.push(c)
+                i++
+            } else if (c == '(') {
+                operatorStack.push(c)
+                i++
+            } else if (c == ')') {
+                while (operatorStack.isNotEmpty() && operatorStack.peek() != '(') {
+                    applyOperator(operatorStack, valueStack)
+                }
+                operatorStack.pop()
+                i++
+            } else {
+                // Ignore other characters
+                i++
+            }
+        }
+
+        while (operatorStack.isNotEmpty()) {
+            applyOperator(operatorStack, valueStack)
+        }
+        return valueStack.pop()
+    }
 }
